363acf1898eb8ccd38823a73fc8e87ddc1a8273c
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-07-13 23:38:10 +0400

    feat: add style for weather divs

--- wheather/src/components/icons/Card/Card.jsx
+++ wheather/src/components/icons/Card/Card.jsx
@@ -1,8 +1,9 @@
-import styles from "./Card.modules.css";
+import "./Card.css";
 
 export function Card({weekDey, icon, temp}) {
   return (
-    <div className={styles.wrapper}>
+   
+    <div className='wrapper'>
       <h3>{weekDey}</h3>
       <div>{icon}</div>
       <p>{temp}</p>
