52e5e9306e5d782e2d333a8ddc79c9590a4973b1
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-07-14 21:35:49 +0400

    feat: add increasing functionality for input number

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,8 +1,13 @@
+import React from "react";
+import { Counter } from "./components/Counter";
+
 
 
 function App() {
-  
-  
+  return(
+    <Counter/>
+  )
+ 
 }
 
-export default App
+export default App;
---
+++ counter-app/src/components/Counter.jsx
@@ -0,0 +1,36 @@
+import React from "react"
+
+
+export class Counter extends React.Component {
+ constructor(props){
+      super(props)
+
+      this.state = {
+        value : ''
+      }
+      this.handleInputChange = this.handleInputChange.bind(this)
+      this.increaseInputValue = this.increaseInputValue.bind(this)
+ }
+ handleInputChange(event){
+    this.setState({value: event.target.value})
+ }
+ increaseInputValue(){
+     this.setState((previousState) => {
+        return {
+            value: Number(previousState.value) + 1
+        }
+     })
+ }
+
+ render() {
+     return (
+        <div>
+            <input type='number' value={this.state.value} onChange={this.handleInputChange}/>
+            <button value='increase' onClick={this.increaseInputValue}>Increase</button>
+            <button value='Decrease' >Decrease</button>
+            <button value='Reset' >Reset</button>
+        </div>
+
+     )
+ }
+}
