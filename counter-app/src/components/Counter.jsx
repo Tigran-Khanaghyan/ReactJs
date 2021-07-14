8f8adb45dad81f95b9d8769093b14be538a1a09d
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-07-14 21:46:13 +0400

    feat: add reset function for input value

--- counter-app/src/components/Counter.jsx
+++ counter-app/src/components/Counter.jsx
@@ -11,7 +11,7 @@ export class Counter extends React.Component {
       this.handleInputChange = this.handleInputChange.bind(this)
       this.increaseInputValue = this.increaseInputValue.bind(this)
       this.decreaseInputValue = this.decreaseInputValue.bind(this)
-
+      this.resetInputValue = this.resetInputValue.bind(this)
  }
  handleInputChange(event){
     this.setState({value: event.target.value})
@@ -31,13 +31,21 @@ export class Counter extends React.Component {
     })
 }
 
+resetInputValue(){
+    this.setState((previousState) => {
+        return {
+            value : Number(previousState.value = 0)
+        }
+    })
+}
+
  render() {
      return (
         <div>
             <input type='number' value={this.state.value} onChange={this.handleInputChange}/>
             <button value='increase' onClick={this.increaseInputValue}>Increase</button>
             <button value='Decrease' onClick={this.decreaseInputValue}>Decrease</button>
-            <button value='Reset' >Reset</button>
+            <button value='Reset' onClick={this.resetInputValue}>Reset</button>
         </div>
 
      )
