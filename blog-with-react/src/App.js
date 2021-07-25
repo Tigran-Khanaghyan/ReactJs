b0bf4a67cd5d465d615380ea45b955b36788f14d
Tigran Khanaghyan <arm.mail1991@gmail.com>
2021-07-25 18:24:10 +0400

    docs(MenuHeader): create header bar with pages

--- blog-with-react/src/App.js
+++ blog-with-react/src/App.js
@@ -1,11 +1,10 @@
 import '@fontsource/roboto'
 import { AccessAlarm, ThreeDRotation } from '@material-ui/icons'
+import MenuHeader from './components/MenuHeader/MenuHeader'
 
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+   <MenuHeader/>
     
   );
 }
---
+++ blog-with-react/src/components/MenuHeader/MenuHeader.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import HomeWorkIcon from '@material-ui/icons/HomeWork';
+import Link from "@material-ui/core/Link";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    margin: 10,
+    color: "white"
+  },
+  linkIcon: {
+      padding: 5
+  }
+}));
+
+export default function MenuHeader() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+          </IconButton>
+          <Typography className={classes.title}>
+            <Link className={classes.linkIcon} href="#"  variant="h6" color="inherit">
+                <HomeWorkIcon/>
+                 Home
+            </Link>
+          </Typography>
+          <Typography variant="h6" className={classes.title}>
+          <Link href="#"  variant="h6" color="inherit">Posts</Link>
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
